56d6e0f44e8a6af484709d4edc1dc1a225b43f91
David Lambert <david.lambert@workday.com>
2025-11-29 19:00:06 +0100

    Initialization of the web UI (nodes).

---
+++ web-ui/.svelte-kit/non-ambient.d.ts
@@ -0,0 +1,52 @@
+
+// this file is generated — do not edit it
+
+
+declare module "svelte/elements" {
+	export interface HTMLAttributes<T> {
+		'data-sveltekit-keepfocus'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-noscroll'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-preload-code'?:
+			| true
+			| ''
+			| 'eager'
+			| 'viewport'
+			| 'hover'
+			| 'tap'
+			| 'off'
+			| undefined
+			| null;
+		'data-sveltekit-preload-data'?: true | '' | 'hover' | 'tap' | 'off' | undefined | null;
+		'data-sveltekit-reload'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-replacestate'?: true | '' | 'off' | undefined | null;
+	}
+}
+
+export {};
+
+
+declare module "$app/types" {
+	export interface AppTypes {
+		RouteId(): "/" | "/[[dbName]]/authentication" | "/[[dbName]]/pullMessages" | "/[[dbName]]/pushMessage" | "/[[dbName]]/[[gridUuid]]/[[uuid]]" | "/[[dbName]]/[[gridUuid]]" | "/[[dbName]]";
+		RouteParams(): {
+			"/[[dbName]]/authentication": { dbName?: string };
+			"/[[dbName]]/pullMessages": { dbName?: string };
+			"/[[dbName]]/pushMessage": { dbName?: string };
+			"/[[dbName]]/[[gridUuid]]/[[uuid]]": { dbName?: string; gridUuid?: string; uuid?: string };
+			"/[[dbName]]/[[gridUuid]]": { dbName?: string; gridUuid?: string };
+			"/[[dbName]]": { dbName?: string }
+		};
+		LayoutParams(): {
+			"/": { dbName?: string; gridUuid?: string; uuid?: string };
+			"/[[dbName]]/authentication": { dbName?: string };
+			"/[[dbName]]/pullMessages": { dbName?: string };
+			"/[[dbName]]/pushMessage": { dbName?: string };
+			"/[[dbName]]/[[gridUuid]]/[[uuid]]": { dbName?: string; gridUuid?: string; uuid?: string };
+			"/[[dbName]]/[[gridUuid]]": { dbName?: string; gridUuid?: string; uuid?: string };
+			"/[[dbName]]": { dbName?: string; gridUuid?: string; uuid?: string }
+		};
+		Pathname(): "/" | `${string}/authentication` & {} | `${string}/authentication/` & {} | `${string}/pullMessages` & {} | `${string}/pullMessages/` & {} | `${string}/pushMessage` & {} | `${string}/pushMessage/` & {} | `${string}${string}${string}` & {} | `${string}${string}${string}/` & {} | `${string}${string}` & {} | `${string}${string}/` & {} | `${string}` & {} | `${string}/` & {};
+		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
+		Asset(): string & {};
+	}
+}
