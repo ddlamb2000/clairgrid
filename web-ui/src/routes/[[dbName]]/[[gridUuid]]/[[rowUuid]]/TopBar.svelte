0638681eff774db254a2aa9b8d6f3fba9af4eceb
David Lambert <david.lambert@workday.com>
2025-12-27 22:04:59 +0100

    Cosmetic.

--- web-ui/src/routes/[[dbName]]/[[gridUuid]]/[[rowUuid]]/TopBar.svelte
+++ web-ui/src/routes/[[dbName]]/[[gridUuid]]/[[rowUuid]]/TopBar.svelte
@@ -6,24 +6,24 @@
   let { context } = $props()
 </script>
 
-<Button size="xs" class="mt-1 mb-1 h-8 w-8 shadow-lg" color="green"
-        onclick={() => context.navigateToGrid(metadata.Grids, "", true, "relationship3", metadata.Grids, context.user.getUserUuid())}>
-  <Icon.GridOutline />
+<Button size="xs" class="mt-1 mb-1 h-8 w-30 shadow-lg" color="green"
+        href={"/" + context.dbName + "/" + metadata.Grids}
+        onclick={() => context.navigateToGrid(metadata.Grids, "")}>
+  <Icon.GridOutline class="me-1" /> Show grids
 </Button>
-<Button size="xs" class="me-1 mt-1 mb-1 h-8 w-8 shadow-lg" color="blue" onclick={() => context.newGrid()}>  
-  <Icon.CirclePlusOutline />
+<Button size="xs" class="mt-1 mb-1 h-8 w-28 shadow-lg" color="blue" onclick={() => context.newGrid()}>  
+  <Icon.CirclePlusOutline class="me-1" /> New grid
 </Button>
 {#each context.dataSets as set}
   {#if set.grid}
-    <Button outline
+    <Button outline size="xs" class="mt-1 me-1 h-8 shadow-lg"
             href={"/" + context.dbName + "/" + set.gridUuid + (set.rowUuid ? "/" + set.rowUuid : "")}
-            size="xs" class="mt-1 me-1 h-8 shadow-lg relative"
             disabled={set.filterColumnName}
             color={!context.userPreferences.showPrompt && context.gridUuid === set.gridUuid && context.rowUuid === (set.rowUuid ?? "") ? "dark" : "light"}
             onclick={() => context.navigateToGrid(set.gridUuid, set.rowUuid)}>
       <DynIcon iconName={set.rowUuid ? "row" : "grid-3x3"}/>
       {#if set.rowUuid && set.rows && set.rows.length > 0}
-      {set.rows[0].displayString} <span class="text-xs">({set.grid.name})</span>
+        {set.rows[0].displayString} <span class="text-xs">({set.grid.name})</span>
       {:else}
         {set.grid.name}
       {/if}
